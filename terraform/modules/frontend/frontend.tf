2c0f8b10ee42e871d778f9a68d9ba7ee0490f585
Luiz Gustavo Gonçalves Brito <lgoncalves@mtrix.com.br>
2025-04-21 22:02:13 -0300

    feat(frontend.tf): add infra s3 + cloudfront

---
+++ terraform/modules/frontend/frontend.tf
@@ -0,0 +1,96 @@
+resource "aws_s3_bucket" "front-bucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_public_access_block" "front-bucket-public-access" {
+  bucket = aws_s3_bucket.front-bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_website_configuration" "front-bucket-website" {
+  bucket = aws_s3_bucket.front-bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+
+}
+
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.front-bucket.id
+  policy = data.aws_iam_policy_document.allow_public_read_access.json
+}
+
+data "aws_iam_policy_document" "allow_public_read_access" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.front-bucket.arn}/*"]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+}
+
+locals {
+  s3_origin_id   = "${var.bucket_name}-origin"
+  s3_domain_name = "${var.bucket_name}.s3-website-us-east-1.amazonaws.com"
+}
+
+resource "aws_cloudfront_distribution" "front-cdn" {
+  enabled = true
+  
+  origin {
+    origin_id                = local.s3_origin_id
+    domain_name              = local.s3_domain_name
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1"]
+    }
+  }
+
+  default_cache_behavior {
+    
+    target_origin_id = local.s3_origin_id
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "all"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  price_class = "PriceClass_200"
+  
+}
